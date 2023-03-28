b3e68ef453314e42d406eed48fa0af4fe0370e76
aspsptyd <devadiroot@gmail.com>
2023-03-28 15:30:11 +0700

    Re-Ekstention file App to JS, dan Re base Codingan App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
